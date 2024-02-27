c377a56f473b9639232ac130f374d740bf4e18a6
enzobersani <enzobersani@hotmail.com>
2024-02-27 02:30:04 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="grid gap-4 sm:grid-cols-2 md:grid-cols-3 container mx-auto">
+      <p v-for="item in list" :key="item.id">{{ item.tittle }}</p>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    setup(){
+      const list = [
+        {id: 1, tittle: "Tailwind CSS"},
+        {id: 2, tittle: "Tailwind CSS"},
+        {id: 3, tittle: "Tailwind CSS"},
+        {id: 4, tittle: "Tailwind CSS"},
+        {id: 5, tittle: "Tailwind CSS"},
+        {id: 6, tittle: "Tailwind CSS"},
+        {id: 7, tittle: "Tailwind CSS"},
+        {id: 8, tittle: "Tailwind CSS"},
+        {id: 9, tittle: "Tailwind CSS"},
+      ];
+  
+      return { list };
+    }
+  };
+  </script>
+  
+  <style>
+  
+  </style>
